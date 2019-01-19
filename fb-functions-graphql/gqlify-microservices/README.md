d70bb55fd7b8fb43741fafab38d531d5ef66e583
jthegedus <james@jthegedus.com>
2019-01-19 13:11:40 +1100

    gqlify as microservice architecture example

---
+++ fb-functions-graphql/gqlify-microservices/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">GQLify on Cloud Functions for FIrebase with a microservice architecture</h1>
---
+++ fb-functions-graphql/gqlify-microservices/users/schemas/uploadSchemas.js
@@ -0,0 +1,43 @@
+const { Storage } = require("@google-cloud/storage");
+const fs = require("fs");
+const path = require("path");
+
+const SERVICE_ACCOUNT_PATH = "./config/firebase-service-account.json";
+const SDL_PROJECT_ID = "blog-post-examples";
+const SDL_BUCKET_NAME = "gqlify_sdls";
+const SCHEMA_EXTENSION = ".graphql";
+
+const storage = new Storage({
+  projectId: SDL_PROJECT_ID,
+  keyFilename: SERVICE_ACCOUNT_PATH,
+});
+
+function getSchemaFiles() {
+  return fs.readdirSync(__dirname).filter(file => {
+    return path.extname(file).toLowerCase() === SCHEMA_EXTENSION;
+  });
+}
+
+async function uploadSchemas(schemaFiles) {
+  console.log(`uploading *${SCHEMA_EXTENSION} files from ${__dirname}`);
+
+  try {
+    await Promise.all(
+      schemaFiles.map(async filename => {
+        console.log(`uploading ${filename}`);
+        await storage
+          .bucket(SDL_BUCKET_NAME)
+          .upload(__dirname + "/" + filename, {
+            gzip: true,
+            metadata: { cacheControl: "no-cache" },
+          });
+      })
+    );
+  } catch (err) {
+    console.error("Upload error: ", err);
+  }
+
+  console.log(`${schemaFiles} uploaded to ${SDL_BUCKET_NAME}.`);
+}
+
+uploadSchemas(getSchemaFiles());
